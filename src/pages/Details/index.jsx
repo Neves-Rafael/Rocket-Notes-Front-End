02f6e006b5cf3b1102a23a828965a419f5181962
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-17 21:35:55 -0300

    :sparkles: Feat a Tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,18 +2,30 @@ import { Container, Links } from "./styles.js";
 import { Button } from "../../components/Button/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
 import { Section } from "../../components/Section/index.jsx";
+import { Tag } from "../../components/Tag/index.jsx";
 
 export function Details() {
   return (
     <Container>
       <Header />
+
       <Section title="Links úteis">
         <Links>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 2</a>
+          </li>
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="Tranquilo" />
+        <Tag title="Importante" />
+        <Tag title="Urgente" />
+      </Section>
+
       <Button title="Back" />
     </Container>
   );
