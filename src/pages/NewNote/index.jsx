856ce88f99b011e3bb04d9bf63a753378be33e19
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-19 14:03:57 -0300

    :sparkles: Feat NoteItem Component

--- src/pages/NewNote/index.jsx
+++ src/pages/NewNote/index.jsx
@@ -2,6 +2,9 @@ import { Container, Form } from "./styles";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 export function NewNote() {
   return (
@@ -16,7 +19,18 @@ export function NewNote() {
 
           <Input placeholder="Title" />
           <Textarea placeholder="Observations" />
+          <Section title="Utils Links">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="New link" />
+          </Section>
 
+          <Section title="Tags">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="New tag" />
+            </div>
+          </Section>
+        <Button title="Save"/>
         </Form>
       </main>
     </Container>
--- src/pages/NewNote/styles.js
+++ src/pages/NewNote/styles.js
@@ -1,29 +1,43 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 
-    display: grid;
-    grid-template-rows: 10.5rem auto;
-    grid-template-areas: "header" "content";
+  display: grid;
+  grid-template-rows: 10.5rem auto;
+  grid-template-areas: "header" "content";
 
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    button {
+      width: auto;
+    }
+  }
 `;
 
 export const Form = styled.form`
-    max-width: 55rem;
-    margin: 3.8rem auto;
+  max-width: 55rem;
+  margin: 3.8rem auto;
 
-    >header{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-        margin-bottom: 3.6rem;
+    margin-bottom: 3.6rem;
 
-        a{
-            font-size: 2rem;
-            color: ${({ theme }) => theme.COLORS.GRAY_100};
-        }
+    a {
+      font-size: 2rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
     }
-`
+  }
+`;
