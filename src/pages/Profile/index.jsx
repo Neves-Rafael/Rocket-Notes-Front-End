0eebbdab32626913e504381aee4ec9dbda3beb20
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-01 18:08:29 -0300

    :sparkles: Feat get user information in profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,8 +3,17 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../../hooks/auth";
 
 export function Profile() {
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
   return (
     <Container>
       <header>
@@ -21,10 +30,32 @@ export function Profile() {
             <input type="file" id="avatar" />
           </label>
         </Avatar>
-        <Input placeholder="Name" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Actual Password" type="password" icon={FiLock} />
-        <Input placeholder="New Password" type="password" icon={FiLock} />
+        <Input
+          placeholder="Name"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Actual Password"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
+        />
+        <Input
+          placeholder="New Password"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordNew(e.target.value)}
+        />
         <Button title="Update"></Button>
       </Form>
     </Container>
