95d81ebd3dff97e31553d727967af9cdc1d6ffb6
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-01 19:52:49 -0300

    :sparkles: Feat update image in Header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,20 @@
 import { Container, Profile, Logout } from "./styles";
 import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
 
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
 
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/Neves-Rafael.png" alt="User Photo" />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Welcome</span>
-          <strong>Rafael Neves</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
       <Logout>
