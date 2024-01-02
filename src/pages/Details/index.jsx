6871d87d739c8d784cb94d1e362adb350fc6f8f9
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-02 19:57:49 -0300

    :sparkles: Feat delete note function

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -19,6 +19,15 @@ export function Details() {
     navigate("/")
   }
 
+  async function handleRemove(){
+    const confirm = window.confirm("Tem certeza que deseja excluir essa nota?");
+
+    if(confirm){
+      await api.delete(`/notes/${params.id}`)
+      navigate("/")
+    }
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
@@ -34,7 +43,7 @@ export function Details() {
       {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText title="Excluir nota" onClick={handleRemove} />
 
             <h1>{data.title}</h1>
             <p>{data.description}</p>
