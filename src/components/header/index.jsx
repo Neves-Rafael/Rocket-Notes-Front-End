5a1baf9d5209b50d3acee63d2f8b2debc12b0fe4
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-02 21:39:03 -0300

    :bug: Fix logout button redirect error

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,9 +3,17 @@ import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
 import avatarPlaceholder from "../../assets/background.jpg";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
   const { signOut, user } = useAuth();
+
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -19,7 +27,7 @@ export function Header() {
         </div>
       </Profile>
       <Logout>
-        <RiShutDownLine onClick={signOut}/>
+        <RiShutDownLine onClick={handleSignOut}/>
       </Logout>
     </Container>
   );
