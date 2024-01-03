cab877492ef76c5988d704075fd288c0fbf1e085
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-02 21:50:48 -0300

    :bug: Fix path app with user on non-existent routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { NewNote } from "../pages/NewNote";
 import { Home } from "../pages/Home";
@@ -12,6 +12,8 @@ export function AppRoutes() {
       <Route path="/new" element={<NewNote />} />
       <Route path="/details/:id" element={<Details />} />
       <Route path="/profile" element={<Profile />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -4,12 +4,13 @@ import { SignUp } from "../pages/SignUp";
 import { SignIn } from "../pages/Signin";
 
 export function AuthRoutes() {
+  const user = localStorage.getItem("@rocketnotes:user");
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
 
-      <Route path="*" element={<Navigate to="/" />} />
+      { !user && <Route path="*" element={<Navigate to="/" />} />}
     </Routes>
   );
 }
